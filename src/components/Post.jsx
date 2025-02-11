4dba7f21e6576005fe043501d877cf8661f02aba
Ailton Oliveira <ailton.oliveira@vocacao.org.br>
2025-02-11 14:59:56 -0300

    criacao e estilizacao componente post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -13,6 +13,7 @@ export function Post() {
         </div>
         <time alt="11 de Fevereiro às 09:36h" datetime="2025-02-11 09:36:00">Publicado há 1h</time>
       </header>
+      
       <div className={styles.content}>
         <p>Fala galeraa 👋</p>
         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
@@ -23,6 +24,13 @@ export function Post() {
           <a href="#">#rocketseat</a>
         </p>
       </div>
+        <form className={styles.commentForm}>
+          <strong>Deixe seu feedback</strong>
+          <textarea placeholder="Deixe um comentário..." />
+          <footer>
+            <button type="submit">Publicar</button>
+          </footer>
+        </form>
     </article>
   )
 }
